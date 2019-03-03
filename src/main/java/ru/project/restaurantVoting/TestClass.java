115ffe6705f2f9762ebc91ee58ca6dd5029e6b73
HammerFall <hammerfall-91@mail.ru>
2019-03-03 10:17:06 +0700

    Settings project, logging, tests

---
+++ src/main/java/ru/project/restaurantVoting/TestClass.java
@@ -0,0 +1,13 @@
+package ru.project.restaurantVoting;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TestClass {
+    private static final Logger log = LoggerFactory.getLogger(TestClass.class);
+
+    public static void main(String[] args) {
+        log.info("test log");
+        System.out.println("test");
+    }
+}
---
+++ src/test/java/ru/project/restaurantVoting/TestClassTest.java
@@ -0,0 +1,14 @@
+package ru.project.restaurantVoting;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TestClassTest {
+
+    @Test
+    public void main() {
+        Assert.assertEquals(1, 1);
+    }
+}
