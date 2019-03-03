8fbb1a6a6c4367b658e8bd2a58baadc1732daeb0
HammerFall <hammerfall-91@mail.ru>
2019-03-03 18:34:43 +0700

    population DB, spring, hibernate

---
+++ src/main/resources/db/populateDB.sql
@@ -0,0 +1,38 @@
+DELETE
+FROM user_roles;
+DELETE
+FROM vote;
+DELETE
+FROM meals;
+DELETE
+FROM restaurants;
+DELETE
+FROM users;
+ALTER SEQUENCE global_seq
+  RESTART WITH 100000;
+
+INSERT INTO users (name, email, password)
+VALUES ('User', 'user@yandex.ru', 'password'),
+       ('Admin', 'admin@gmail.com', 'admin');
+
+INSERT INTO user_roles (role, user_id)
+VALUES ('ROLE_USER', 100000),
+       ('ROLE_ADMIN', 100001);
+
+INSERT INTO restaurants (name, address)
+VALUES ('Арарат', 'ул. Ноябрьская, 58/1'),
+       ('Забой', 'ул. Институтская, 2');
+
+INSERT INTO meals (date_meal, description, restaurant_id, price)
+VALUES (CURRENT_DATE, 'Салат', 100002, 80),
+       (CURRENT_DATE, 'Борщ', 100002, 100),
+       (CURRENT_DATE, 'Паста', 100002, 120),
+       (CURRENT_DATE, 'Кофе', 100002, 60),
+       (CURRENT_DATE, 'Амлет', 100003, 70),
+       (CURRENT_DATE, 'Щи', 100003, 90),
+       (CURRENT_DATE, 'Плов', 100003, 150),
+       (CURRENT_DATE, 'Чай', 100003, 50);
+
+INSERT INTO vote (date_vote, user_id, restaurant_id)
+VALUES (CURRENT_DATE, 100000, 100002),
+       (CURRENT_DATE, 100001, 100003);
