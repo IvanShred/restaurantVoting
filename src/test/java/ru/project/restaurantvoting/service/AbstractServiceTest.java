fe90a0cdb44507551ddb8e5fa5020fb45b9dfcd5
Кухта Иван <kuhtaia@it2g.tu>
2019-09-01 16:46:03 +0700

    removed timingExtension

--- src/test/java/ru/project/restaurantvoting/service/AbstractServiceTest.java
+++ src/test/java/ru/project/restaurantvoting/service/AbstractServiceTest.java
@@ -1,16 +1,13 @@
 package ru.project.restaurantvoting.service;
 
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlConfig;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
-import ru.project.restaurantvoting.TimingExtension;
 
 @SpringJUnitConfig(locations = {
         "classpath:spring/spring-app.xml",
         "classpath:spring/spring-db.xml"
 })
 @Sql(scripts = "classpath:db/populateDB.sql", config = @SqlConfig(encoding = "UTF-8"))
-@ExtendWith(TimingExtension.class)
 abstract class AbstractServiceTest {
 }
